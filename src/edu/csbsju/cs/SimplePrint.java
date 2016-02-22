88c2331a6e95e9f338e7aa7594b623b838c057eb
irahhal <imad.rahal@gmail.com>
2016-02-22 13:32:48 -0600

    First Import to GIT

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author irahal
+ *
+ */
+public class SimplePrint implements ActionListener {
+
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<args.length;i++){
+			System.out.println("args[ " + i + " ] = " + args[i] );	
+		}
+		
+	}
+
+}
